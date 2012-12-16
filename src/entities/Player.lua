b1ef4447e04e61d2a664c8757c17932a8f16eed1
kikito <kikito@gmail.com>
2012-12-16 10:52:26 +0100

    made player a bit faster

--- src/entities/Player.lua
+++ src/entities/Player.lua
@@ -5,7 +5,7 @@ local Being = require.relative(..., 'Being')
 local Player = class('Player', Being)
 
 function Player:initialize(x,y)
-  Being.initialize(self, x, y, 16, 16, 70)
+  Being.initialize(self, x, y, 16, 16, 85)
 end
 
 function Player:die()
